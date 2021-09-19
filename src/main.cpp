3bea5d82560756a65cd5f252966ad9b7b752b977
squid233 <513508220@qq.com>
2021-09-19 17:44:45 +0800

    0.2.0 ready

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <windows.h>
 
 #include <iostream>
-#include <cstdarg>
 
 #include "glad/gl.h"
 #define GLFW_INCLUDE_NONE
@@ -20,10 +19,13 @@ constexpr int MBL = GLFW_MOUSE_BUTTON_LEFT;
 constexpr int MBM = GLFW_MOUSE_BUTTON_MIDDLE;
 constexpr int MBR = GLFW_MOUSE_BUTTON_RIGHT;
 
-using namespace Real2D;
+using Real2D::Block;
+using Real2D::Blocks;
+using Real2D::Timer;
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::string;
 
 constexpr int DEF_W = 854;
 constexpr int DEF_H = 480;
@@ -150,30 +152,11 @@ void fbcb(Window window_, int width_, int height_) {
     glLoadIdentity();
 }
 
-void close() {
-    if (world) {
-        free(world);
-    }
-    if (blocks > 0) {
-        glDeleteTextures(1, &blocks);
-    }
-    glfwSetKeyCallback(window, nullptr);
-    glfwSetFramebufferSizeCallback(window, nullptr);
-    glfwSetCursorPosCallback(window, nullptr);
-    glfwSetMouseButtonCallback(window, nullptr);
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    glfwSetErrorCallback(nullptr);
-}
-
-int loadTexture() {
+void loadTexture() {
     int w = 0;
     int h = 0;
     int comp = 0;
     stbi_uc* tex = stbi_load_out("res/block/blocks0.png", &w, &h, &comp, STBI_rgb_alpha);
-    if (!tex) {
-        return -1;
-    }
     glGenTextures(1, &blocks);
     glBindTexture(GL_TEXTURE_2D, blocks);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -189,92 +172,6 @@ int loadTexture() {
         tex
     );
     stbi_image_free(tex);
-    return 0;
-}
-
-int init() {
-    glfwSetErrorCallback(errcb);
-    if (!glfwInit()) {
-        cerr << "Unable to initialize GLFW" << endl;
-        return -1;
-    }
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-    window = glfwCreateWindow(DEF_W, DEF_H, "Real2D", nullptr, nullptr);
-    if (window == nullptr) {
-        glfwTerminate();
-        cerr << "Unable to create GLFW window" << endl;
-        return -1;
-    }
-
-    glfwSetKeyCallback(window, keycb);
-    glfwSetFramebufferSizeCallback(window, fbcb);
-    glfwSetCursorPosCallback(window, cpcb);
-    glfwSetMouseButtonCallback(window, mbcb);
-
-    const GLFWvidmode* vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-    if (vidmode) {
-        glfwSetWindowPos(window,
-            (vidmode->width - width) >> 1,
-            (vidmode->height - height) >> 1);
-    }
-
-    int w = 0;
-    int h = 0;
-    int comp = 0;
-    stbi_uc* icon16 = stbi_load_out("res/icon16.png", &w, &h, &comp, STBI_rgb_alpha);
-    if (!icon16) {
-        return -1;
-    }
-    stbi_uc* icon32 = stbi_load_out("res/icon32.png", &w, &h, &comp, STBI_rgb_alpha);
-    if (!icon32) {
-        return -1;
-    }
-    stbi_uc* icon48 = stbi_load_out("res/icon48.png", &w, &h, &comp, STBI_rgb_alpha);
-    if (!icon48) {
-        return -1;
-    }
-    GLFWimage img[] = {
-        { 16, 16, icon16 },
-        { 32, 32, icon32 },
-        { 48, 48, icon48 }
-    };
-    glfwSetWindowIcon(window, 3, img);
-    stbi_image_free(icon16);
-    stbi_image_free(icon32);
-    stbi_image_free(icon48);
-
-    glfwMakeContextCurrent(window);
-    gladLoadGL(glfwGetProcAddress);
-    glfwSwapInterval(1);
-
-    fbcb(window, DEF_W, DEF_H);
-    glEnable(GL_TEXTURE_2D);
-    for (int z = 0; z < WORLD_D; ++z) {
-        for (int x = 0; x < WORLD_W; ++x) {
-            for (int y = 0; y < WORLD_H; ++y) {
-                int p = WORLD_BLOCK(x, y, z);
-                if (y < 3) {
-                    world[p] = BLOCK(STONE);
-                }
-                else if (y == 3) {
-                    world[p] = BLOCK(GRASS_BLOCK);
-                }
-                else {
-                    world[p] = AIR_BLOCK;
-                }
-            }
-        }
-    }
-
-    int i = loadTexture();
-    if (i) {
-        close();
-        return i;
-    }
-    timer.advanceTime();
-
-    glfwShowWindow(window);
-    return 0;
 }
 
 void renderWorld() {
@@ -383,7 +280,107 @@ unloop:
     }
 }
 
-void render() {
+namespace Real2D {
+    class Real2D {
+    public:
+        void start();
+        void run();
+        void render();
+        void tick(double delta);
+        ~Real2D();
+    };
+}
+void Real2D::Real2D::start() {
+    glfwSetErrorCallback(errcb);
+    if (!glfwInit()) {
+        throw "Unable to initialize GLFW";
+    }
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    window = glfwCreateWindow(DEF_W, DEF_H, "Real2D", nullptr, nullptr);
+    if (window == nullptr) {
+        glfwTerminate();
+        throw "Unable to create GLFW window";
+    }
+
+    glfwSetKeyCallback(window, keycb);
+    glfwSetFramebufferSizeCallback(window, fbcb);
+    glfwSetCursorPosCallback(window, cpcb);
+    glfwSetMouseButtonCallback(window, mbcb);
+
+    const GLFWvidmode* vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    if (vidmode) {
+        glfwSetWindowPos(window,
+            (vidmode->width - width) >> 1,
+            (vidmode->height - height) >> 1);
+    }
+
+    int w = 0;
+    int h = 0;
+    int comp = 0;
+    stbi_uc* icon16 = stbi_load_out("res/icon16.png", &w, &h, &comp, STBI_rgb_alpha);
+    stbi_uc* icon32 = stbi_load_out("res/icon32.png", &w, &h, &comp, STBI_rgb_alpha);
+    stbi_uc* icon48 = stbi_load_out("res/icon48.png", &w, &h, &comp, STBI_rgb_alpha);
+    GLFWimage img[] = {
+        { 16, 16, icon16 },
+        { 32, 32, icon32 },
+        { 48, 48, icon48 }
+    };
+    glfwSetWindowIcon(window, 3, img);
+    stbi_image_free(icon16);
+    stbi_image_free(icon32);
+    stbi_image_free(icon48);
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    fbcb(window, DEF_W, DEF_H);
+    glClearColor(0.4f, 0.6f, 0.9f, 1.0f);
+    glEnable(GL_TEXTURE_2D);
+    for (int z = 0; z < WORLD_D; ++z) {
+        for (int x = 0; x < WORLD_W; ++x) {
+            for (int y = 0; y < WORLD_H; ++y) {
+                int p = WORLD_BLOCK(x, y, z);
+                if (y < 3) {
+                    world[p] = BLOCK(STONE);
+                }
+                else if (y == 3) {
+                    world[p] = BLOCK(GRASS_BLOCK);
+                }
+                else {
+                    world[p] = AIR_BLOCK;
+                }
+            }
+        }
+    }
+
+    loadTexture();
+    timer.advanceTime();
+
+    glfwShowWindow(window);
+
+    run();
+}
+void Real2D::Real2D::run() {
+    double lastTime = glfwGetTime() * 1000;
+    int frames = 0;
+    while (!glfwWindowShouldClose(window)) {
+        timer.advanceTime();
+        double delta = timer.delta;
+        for (int i = 0; i < timer.ticks; ++i) {
+            tick(delta);
+        }
+        render();
+        glfwPollEvents();
+        ++frames;
+        while (glfwGetTime() * 1000 >= lastTime + 1000) {
+            timer.fps = frames;
+            lastTime += 1000;
+            frames = 0;
+        }
+    }
+}
+void Real2D::Real2D::render() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -392,8 +389,7 @@ void render() {
     glDisable(GL_BLEND);
     glfwSwapBuffers(window);
 }
-
-void tick(double delta) {
+void Real2D::Real2D::tick(double delta) {
     float step = PLAYER_STEP * (float)delta;
     if (isKeyDown(GLFW_KEY_W)) {
         playerZ -= step;
@@ -422,6 +418,21 @@ void tick(double delta) {
         playerX += step;
     }
 }
+Real2D::Real2D::~Real2D() {
+    if (world) {
+        free(world);
+    }
+    if (blocks > 0) {
+        glDeleteTextures(1, &blocks);
+    }
+    glfwSetKeyCallback(window, nullptr);
+    glfwSetFramebufferSizeCallback(window, nullptr);
+    glfwSetCursorPosCallback(window, nullptr);
+    glfwSetMouseButtonCallback(window, nullptr);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    glfwSetErrorCallback(nullptr);
+}
 
 int WINAPI WinMain(
     HINSTANCE hInstance,
@@ -429,28 +440,7 @@ int WINAPI WinMain(
     LPSTR     lpCmdLine,
     int       nShowCmd
 ) {
-    int i = init();
-    if (i) {
-        return i;
-    }
-    glClearColor(0.4f, 0.6f, 0.9f, 1.0f);
-    double lastTime = glfwGetTime() * 1000;
-    int frames = 0;
-    while (!glfwWindowShouldClose(window)) {
-        timer.advanceTime();
-        double delta = timer.delta;
-        for (int i = 0; i < timer.ticks; ++i) {
-            tick(delta);
-        }
-        render();
-        glfwPollEvents();
-        ++frames;
-        while (glfwGetTime() * 1000 >= lastTime + 1000) {
-            timer.fps = frames;
-            lastTime += 1000;
-            frames = 0;
-        }
-    }
-    close();
+    Real2D::Real2D real2d;
+    real2d.start();
     return 0;
 }
